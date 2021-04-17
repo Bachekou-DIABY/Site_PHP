6f030fc499e4f7593e0d36b259462c76d1ada3e9
DIABY Bachekou <bac.diaby@gmail.com>
2021-04-17 17:28:09 +0200

    externalize in .php files connected pages

---
+++ User_connected/User_validated.php
@@ -0,0 +1,56 @@
+<div class="grid-container">
+  <div class="logo"></div>
+  <div class="subtitle">
+    <h1>Ma Banque en ligne</h1>
+  </div>
+  <div class="navigation">
+      <ul class="navbar">
+        <li class= "btn btn-primary">
+          <a href="/ECF-banque/Accueil/index.html">Accueil</a>
+        </li>
+      </ul>
+    </div>
+    <?php
+  require_once 'C:/xampp/htdocs/ECF-Banque/Ressources/db.php';
+  class MyDB extends SQLite3
+  {
+      public function __construct()
+      {
+          $this->open('C:/xampp/htdocs/ECF-Banque/Ressources/ECF-Banque.db');
+      }
+  }
+  $db = new MyDB();
+
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  $req = $db->prepare('SELECT * FROM user WHERE email = :email');
+  $req->bindValue(':email', $email, SQLITE3_TEXT);
+  $res = $req->execute()->fetchArray(SQLITE3_ASSOC);
+
+  if (!$res) {
+      echo 'Mauvais identifiant !';
+  } else {
+      $isPasswordCorrect = password_verify($password, $res['password']);
+      if ($isPasswordCorrect) {
+          session_start();
+          $_SESSION['id'] = $res['email'];
+          $_SESSION['email'] = $res['email'];
+          $_SESSION['first_name'] = $res['first_name'];
+          $_SESSION['last_name'] = $res['last_name'];
+          echo 'Vous êtes connecté !';
+      } else {
+          echo 'Mauvais mot de passe !';
+      }
+  }
+  ?>
+  <div class="content">
+    <h1>Bienvenue sur votre page d'accueil</h1>
+    <div>
+      <p> Montant : </p>
+    </div>
+  </div>
+  <div class="footer">
+    <p><b>Tous droits reservés</b></p>
+  </div>
+</div>
--- User_connected/index.php
+++ User_connected/index.php
@@ -10,65 +10,26 @@
   <title>ECF Banque</title>
 </head>
 <body>
-<div class="grid-container">
-  <div class="logo"></div>
-  <div class="subtitle">
-    <h1>Ma Banque en ligne</h1>
-  </div>
-  <div class="navigation">
-      <ul class="navbar">
-        <li class= "btn btn-primary">
-          <a href="/ECF-banque/Accueil/index.html">Accueil</a>
-        </li>
-      </ul>
-    </div>
-
-    <?php
+<?php
   require_once 'C:/xampp/htdocs/ECF-Banque/Ressources/db.php';
-  class MyDB extends SQLite3
+  class DB extends SQLite3
   {
       public function __construct()
       {
           $this->open('C:/xampp/htdocs/ECF-Banque/Ressources/ECF-Banque.db');
       }
   }
-  $db = new MyDB();
-
+  $db = new DB();
   $email = $_POST['email'];
-  $password = $_POST['password'];
 
-  $req = $db->prepare('SELECT * FROM user WHERE email = :email');
+  $req = $db->prepare('SELECT is_validated FROM user WHERE email = :email');
   $req->bindValue(':email', $email, SQLITE3_TEXT);
-  $res = $req->execute()->fetchArray(SQLITE3_ASSOC);
-  var_dump($res);
-
-  if (!$res) {
-      echo 'Mauvais identifiant !';
+  $is_validated = $req->execute()->fetchArray(SQLITE3_ASSOC)['is_validated'];
+  if (0 === $is_validated) {
+      require_once 'C:/xampp/htdocs/ECF-Banque/User_connected/User_not_validated.php';
   } else {
-      $isPasswordCorrect = password_verify($password, $res['password']);
-      if ($isPasswordCorrect) {
-          session_start();
-          $_SESSION['id'] = $res['email'];
-          $_SESSION['email'] = $res['email'];
-          $_SESSION['first_name'] = $res['first_name'];
-          $_SESSION['last_name'] = $res['last_name'];
-          echo 'Vous êtes connecté !';
-          var_dump($_SESSION);
-      } else {
-          echo 'Mauvais mot de passe !';
-      }
+      require_once 'C:/xampp/htdocs/ECF-Banque/User_connected/User_validated.php';
   }
   ?>
-
-  <div class="content">
-    <h1>Bienvenue sur votre page d'accueil</h1>
-    <div>
-
-    </div>
-  </div>
-  <div class="footer">
-    <p><b>Tous droits reservés</b></p>
-  </div>
-</div>
 </body>
 </html>
