c42207b12c927e50ef1e576cbec93a65a9b45726
DIABY Bachekou <bac.diaby@gmail.com>
2021-04-28 08:34:55 +0200

    add modify info success folder

---
+++ Modify_login_info_success/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../bootstrap/bootstrap-4.4.1-dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.CSS">
+  <link rel="stylesheet" href="../CSS/Template.css">
+  <title>ECF Banque</title>
+</head>
+<body>
+<?php
+require_once '../Ressources/db.php';
+$db = new DB();
+session_start();
+$admin_id = $_SESSION['id'];
+$req = $db->prepare('UPDATE banker SET first_connexion=1 WHERE id=:admin_id');
+$req->bindValue(':admin_id', $admin_id, SQLITE3_INTEGER);
+$req->execute();
+?>
+<div class="grid-container">
+  <div class="logo"></div>
+  <div class="subtitle">
+    <h1>Ma Banque en ligne</h1>
+  </div>
+  <div class="navigation">
+      <ul class="navbar">
+        <li class= "btn btn-primary">
+          <a href="../Admin_connected/Admin_connected.php">Retour</a>
+        </li>
+      </ul>
+    </div>
+  <div class="content">
+    <h1>Les informations ont bien été modifiées</h1>
+    <a href="../Admin_connexion/index.php">Se connecter a nouveau</a>
+  </div>
+  <div class="footer">
+    <p><b>Tous droits reservés</b></p>
+  </div>
+</div>
+</body>
+</html>
