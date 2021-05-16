09c6db4a4033d571438b0727bbb5857aaf617156
DIABY Bachekou <bac.diaby@gmail.com>
2021-05-16 23:29:53 +0200

    add database for heroku

--- Ressources/db.php
+++ Ressources/db.php
@@ -7,10 +7,20 @@ class DB extends mysqli
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         parent::__construct($host, $user, $pass, $db, $port, $socket);
         $this->set_charset($charset);
-        $this->connect('us-cdbr-east-03.cleardb.com', 'b1be912b1df6a8', '507f8e59', 'heroku_85e4eb877a354da', 3306, null);
+        $this->connect($host, $user, $pass, $db, $port, $socket);
     }
 }
 function db_connect()
 {
-    return new DB('us-cdbr-east-03.cleardb.com', 'b1be912b1df6a8', '507f8e59', 'heroku_85e4eb877a354da', 3306, null, 'utf8mb4');
+    $dbopts = parse_url(getenv('DATABASE_URL'));
+
+    return new DB(
+        $dbopts['host'],
+        $dbopts['user'],
+        $dbopts['pass'],
+        ltrim($dbopts['path'], '/'),
+        $dbopts['port'],
+        null,
+        'utf8mb4'
+    );
 }
