055e2602d57d88bf0f24a9baab3e2566ac2118b0
DIABY Bachekou <bac.diaby@gmail.com>
2021-05-16 03:02:58 +0200

    modified db

--- Ressources/db.php
+++ Ressources/db.php
@@ -7,10 +7,10 @@ class DB extends mysqli
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         parent::__construct($host, $user, $pass, $db, $port, $socket);
         $this->set_charset($charset);
-        $this->connect('localhost', 'root', 'root', 'ecf-banque', 3306, null);
+        $this->connect('localhost', 'bb8efa8242d022', '93bb7911', 'ecf-banque', 3306, null);
     }
 }
 function db_connect()
 {
-    return new DB('localhost', 'root', 'root', 'ecf-banque', 3306, null, 'utf8mb4');
+    return new DB('localhost', 'bb8efa8242d022', '93bb7911', 'ecf-banque', 3306, null, 'utf8mb4');
 }
