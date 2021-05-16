c144c9a89947687fa0835e79b96d911bcce6788a
DIABY Bachekou <bac.diaby@gmail.com>
2021-05-16 23:58:55 +0200

    add database for heroku

--- Ressources/db.php
+++ Ressources/db.php
@@ -5,9 +5,9 @@ class DB extends mysqli
     public function __construct($host, $user, $pass, $db, $port, $socket, $charset)
     {
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-        parent::__construct($host, $user, $pass, $db, $port, $socket);
+        parent::__construct($host, $user, $pass, $db, 3306, $socket);
         $this->set_charset($charset);
-        $this->connect($host, $user, $pass, $db, $port, $socket);
+        $this->connect($host, $user, $pass, $db, 3306, $socket);
     }
 }
 function db_connect()
