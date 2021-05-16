e0a910a7e0a3a0dea3ceab042753a9523b778451
DIABY Bachekou <bac.diaby@gmail.com>
2021-05-16 02:35:52 +0200

    add cdn bootstrap

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name=" description "  content="">
 
-  <link rel="stylesheet" href="../bootstrap/bootstrap-4.4.1-dist/css/bootstrap.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-  <link rel="stylesheet" href="../CSS/style.css">
+  <link rel="stylesheet" href="./CSS/style.css">
   <title>ECF-Banque</title>
 </head>
 <body>
@@ -31,10 +31,10 @@
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <div class="navbar-nav nav-justified w-100 ">
-              <a class="nav-link active" href="../Homepage/index.php">Accueil</a>
-              <a class="nav-link" href="../User_connexion/index.php">Connexion</a>
-              <a class="nav-link" href="../Admin_connexion/index.php">Connexion Administrateur</a>
-              <a class="nav-link" href="../Inscription/index.php">Inscription</a>
+              <a class="nav-link active" href="index.php">Accueil</a>
+              <a class="nav-link" href="./User_connexion/index.php">Connexion</a>
+              <a class="nav-link" href="./Admin_connexion/index.php">Connexion Administrateur</a>
+              <a class="nav-link" href="./Inscription/index.php">Inscription</a>
             </div>
           </div>    
         </div>
