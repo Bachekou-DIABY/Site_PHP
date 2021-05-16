f7f83d2d403a7672c91c293a2fbad77e59e7bdf7
DIABY Bachekou <bac.diaby@gmail.com>
2021-05-16 23:39:04 +0200

    add database for heroku

--- Ressources/setup.php
+++ Ressources/setup.php
@@ -1,45 +1,45 @@
 <?php
 
-$sql = "CREATE TABLE `banker` (
-  `id` int NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `first_connexion` tinyint NOT NULL DEFAULT '0'
+$sql = "CREATE TABLE banker (
+  id int NOT NULL,
+  last_name varchar(50) NOT NULL,
+  first_name varchar(50) NOT NULL,
+  email varchar(50) NOT NULL,
+  password varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  first_connexion tinyint NOT NULL DEFAULT '0'
 ) DEFAULT CHARSET=utf8mb3;
 
-CREATE TABLE `beneficiary` (
-  `id` int NOT NULL,
-  `last_name` varchar(30) NOT NULL,
-  `first_name` varchar(30) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `BankID` varchar(10) NOT NULL
+CREATE TABLE beneficiary (
+  id int NOT NULL,
+  last_name varchar(30) NOT NULL,
+  first_name varchar(30) NOT NULL,
+  email varchar(50) NOT NULL,
+  BankID varchar(10) NOT NULL
 ) DEFAULT CHARSET=utf8mb3;
 
-CREATE TABLE `users` (
-  `id` int NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `birthdate` date NOT NULL,
-  `adress` varchar(100) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `PASSWORD` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `BankID` varchar(30) DEFAULT NULL,
-  `identity` varchar(100) DEFAULT NULL,
-  `is_validated` tinyint NOT NULL DEFAULT '0',
-  `ask_delete` tinyint NOT NULL DEFAULT '0',
-  `amount` float NOT NULL DEFAULT '0'
+CREATE TABLE users (
+  id int NOT NULL,
+  last_name varchar(50) NOT NULL,
+  first_name varchar(50) NOT NULL,
+  birthdate date NOT NULL,
+  adress varchar(100) NOT NULL,
+  email varchar(50) NOT NULL,
+  PASSWORD varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  BankID varchar(30) DEFAULT NULL,
+  identity varchar(100) DEFAULT NULL,
+  is_validated tinyint NOT NULL DEFAULT '0',
+  ask_delete tinyint NOT NULL DEFAULT '0',
+  amount float NOT NULL DEFAULT '0'
 ) DEFAULT CHARSET=utf8mb3;
 
-ALTER TABLE `banker`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE banker
+  ADD PRIMARY KEY (id);
 
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`) USING BTREE;
+ALTER TABLE users
+  ADD PRIMARY KEY (id) USING BTREE;
 
-ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;";
+ALTER TABLE users
+  MODIFY id int NOT NULL AUTO_INCREMENT;";
 
 require_once '../Ressources/db.php';
 
