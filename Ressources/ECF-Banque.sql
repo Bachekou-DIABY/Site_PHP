4a42c36147c6f3048f4cd5bdaac667beed36836d
DIABY Bachekou <bac.diaby@gmail.com>
2021-05-17 12:38:10 +0200

    add database for heroku

---
+++ Ressources/ECF-Banque.sql
@@ -0,0 +1,116 @@
+CREATE DATABASE  IF NOT EXISTS `heroku_85e4eb877a354da` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `heroku_85e4eb877a354da`;
+-- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
+--
+-- Host: us-cdbr-east-03.cleardb.com    Database: heroku_85e4eb877a354da
+-- ------------------------------------------------------
+-- Server version	5.6.50-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `banker`
+--
+
+DROP TABLE IF EXISTS `banker`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `banker` (
+  `id` int(11) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `first_connexion` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `banker`
+--
+
+LOCK TABLES `banker` WRITE;
+/*!40000 ALTER TABLE `banker` DISABLE KEYS */;
+INSERT INTO `banker` VALUES (1,'Dupont','Jean','dupont.jean@gmail.com','$2y$10$OtevuZqH6PHC/hvCfYXY/eIrtefeKWYwYJUWMuHvrxNftJIRyPHbC',1),(2,'Dupont','Jeanne','dupont.jeanne@gmail.com','$2y$10$OtevuZqH6PHC/hvCfYXY/eIrtefeKWYwYJUWMuHvrxNftJIRyPHbC',1),(3,'Dupont','Pierre','dupont.pierre@gmail.com','$2y$10$OtevuZqH6PHC/hvCfYXY/eIrtefeKWYwYJUWMuHvrxNftJIRyPHbC',1),(4,'Dupont','Paul','dupont.paul@gmail.com','$2y$10$OtevuZqH6PHC/hvCfYXY/eIrtefeKWYwYJUWMuHvrxNftJIRyPHbC',1),(5,'Dupont','Jacques','dupont.jacques@gmail.com','$2y$10$OtevuZqH6PHC/hvCfYXY/eIrtefeKWYwYJUWMuHvrxNftJIRyPHbC',1);
+/*!40000 ALTER TABLE `banker` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `beneficiary`
+--
+
+DROP TABLE IF EXISTS `beneficiary`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `beneficiary` (
+  `id` int(11) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `BankID` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `beneficiary`
+--
+
+LOCK TABLES `beneficiary` WRITE;
+/*!40000 ALTER TABLE `beneficiary` DISABLE KEYS */;
+/*!40000 ALTER TABLE `beneficiary` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `last_name` varchar(50) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `birthdate` date NOT NULL,
+  `adress` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `PASSWORD` varchar(60) NOT NULL,
+  `BankID` varchar(30) DEFAULT NULL,
+  `identity` varchar(100) DEFAULT NULL,
+  `is_validated` tinyint(4) NOT NULL DEFAULT '0',
+  `ask_delete` tinyint(4) NOT NULL DEFAULT '0',
+  `amount` float NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (4,'DIABY','Bachekou','2021-05-14','23 rue saint eusebe','bac.diaby@gmail.com','$2y$10$uCfB4F57DwUZ8OpgObt7suaY3kkFGDkqbNcE0DhbRojPcgL6nzPC.','REwz7dsuY9','Photo.png',0,0,0),(14,'DIABY','Bachekou','2021-05-07','23 rue saint eusebe','d@gmail.com','$2y$10$cBsBzY8s0oQ3S3GIXPF5S.ljEbmMWV27sFN76V0LdqVGHsVW3HFDO','0nGlKzbBI7','Photo.png',0,0,0),(24,'DIABY','Bachekou','2021-05-05','23 rue saint eusebe','bac13112000@gmail.com','$2y$10$dbVK3U2oebnM3thmY9RlQeIqKh9LXYvgrOAh3jupNDD48hkXE2.Zy','PEaRl4cnNp','Photo.png',0,0,0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-17 12:35:26
