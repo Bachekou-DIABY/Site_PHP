43dbf33bf40a3a52bf01ad82a63c8fe6ebfc4b66
DIABY Bachekou <bac.diaby@gmail.com>
2021-05-17 16:16:30 +0200

    add database for heroku

--- User_add_beneficiary/add_beneficiary.php
+++ User_add_beneficiary/add_beneficiary.php
@@ -8,7 +8,7 @@ $last_name = $_POST['last_name'];
 $first_name = $_POST['first_name'];
 $bankID = $_POST['BankID'];
 
-$stmt = $db->prepare('INSERT INTO beneficiary(user_id,last_name,first_name,BankID)
+$stmt = $db->prepare('INSERT INTO beneficiary(id,last_name,first_name,BankID)
 VALUES(?,?,?,?)');
 $stmt->bind_param('isss', $user_id, $last_name, $first_name, $bankID);
 $stmt->execute();
