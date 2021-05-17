ebe28a38f2a19f72fa4efc09d82f36990b926d55
DIABY Bachekou <bac.diaby@gmail.com>
2021-05-17 12:19:17 +0200

    add database for heroku

--- Ressources/Bank_ID_Generator.php
+++ Ressources/Bank_ID_Generator.php
@@ -3,9 +3,8 @@
 function generateChain($length, $caracters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
 {
     $chain = '';
-    $max = mb_strlen($caracters) - 1;
     for ($i = 0; $i < $length; ++$i) {
-        $chain .= $caracters[random_int(0, $max)];
+        $chain .= $caracters[random_int(0, 61)];
     }
 
     return $chain;
